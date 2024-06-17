e8d41f0a02d608e60f0c25962e602c2486859e3c
Intro <intro-iu@outlook.com>
2024-06-17 23:22:22 +0800

    modified:   plugins/decoration/x11shadow.cpp

--- plugins/decoration/x11shadow.cpp
+++ plugins/decoration/x11shadow.cpp
@@ -8,11 +8,11 @@ static xcb_atom_t internAtom(const char *name, bool only_if_exists)
     if (!name || *name == 0)
         return XCB_NONE;
 
-    if (QGuiApplication::platformName() != "xcb")
+    if (QGuiApplication::platformName() != QLatin1String("xcb")
         return XCB_NONE;
 
-    xcb_intern_atom_cookie_t cookie = xcb_intern_atom(QX11Application::connection() , only_if_exists, strlen(name), name);
-    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(QX11Application::connection() , cookie, 0);
+    xcb_intern_atom_cookie_t cookie = xcb_intern_atom(QNativeInterface::QX11Application::connection(), only_if_exists, strlen(name), name);
+    xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(QNativeInterface::QX11Application::connection(), cookie, 0);
 
     if (!reply)
         return XCB_NONE;
